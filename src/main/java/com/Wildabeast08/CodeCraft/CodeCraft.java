86bd674998e93c0a870bf1bb8c92cc74b944e4a8
william <wilomont@canterburyschool.org>
2024-05-20 13:30:41 -0400

    funniy

--- src/main/java/com/Wildabeast08/CodeCraft/CodeCraft.java
+++ src/main/java/com/Wildabeast08/CodeCraft/CodeCraft.java
@@ -1,4 +1,4 @@
-package com.your_name.your_mod_id;
+package com.Wildabeast08.codecraft;
 
 import com.mojang.logging.LogUtils;
 import com.your_name.your_mod_id.registry.YMIBlocks;
@@ -9,19 +9,19 @@ import net.neoforged.fml.common.Mod;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(YourModID.MOD_ID)
-public class YourModID
+@Mod(CodeCraft.MOD_ID)
+public class CodeCraft
 {
     // Define mod id in a common place for everything to reference
-    public static final String MOD_ID = "your_mod_id";
+    public static final String MOD_ID = "CodeCraft";
     // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    public YourModID(IEventBus modEventBus) {
+    public CodeCraft(IEventBus modEventBus) {
 
-        YMIItems.register(modEventBus);
-        YMIBlocks.register(modEventBus);
-        YMICreativeModeTabs.register(modEventBus);
+        CCItems.register(modEventBus);
+        CCBlocks.register(modEventBus);
+        CCCreativeModeTabs.register(modEventBus);
 
     }
 
